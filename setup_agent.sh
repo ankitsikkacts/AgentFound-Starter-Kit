e67046c24e708339e22885f2a7e7b4c29d5b80dd
ankitsikkacts <ankit.sikka@cognizant.com>
2025-07-22 16:52:02 +0530

    Update setup_agent.sh

--- setup_agent.sh
+++ setup_agent.sh
@@ -1,4 +1,7 @@
-aws iam create-user --user-name test-iam-user
-aws iam attach-user-policy --user-name test-iam-user --policy-arn arn:aws:iam::aws:policy/AmazonBedrockFullAccess
-aws iam create-access-key --user-name test-iam-user
+aws iam create-user --user-name agent-iam-user
+aws iam attach-user-policy --user-name agent-iam-user --policy-arn arn:aws:iam::aws:policy/AmazonBedrockFullAccess
+aws iam create-access-key --user-name agent-iam-user
 
+aws iam create-user --user-name math-iam-user
+aws iam attach-user-policy --user-name math-iam-user --policy-arn arn:aws:iam::aws:policy/AmazonBedrockFullAccess
+aws iam create-access-key --user-name math-iam-user

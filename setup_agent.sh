4bba29869c4ecd3b3f756a379cddee31ba95ab06
ankitsikkacts <ankit.sikka@cognizant.com>
2025-07-23 22:17:25 +0530

    Update setup_agent.sh

--- setup_agent.sh
+++ setup_agent.sh
@@ -17,6 +17,26 @@ aws iam put-user-policy \
     ]
   }'
 
+aws iam put-user-policy \
+    --user-name agent-iam-user \
+    --policy-name BedrockLogsAccess \
+    --policy-document '{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "logs:FilterLogEvents",
+                    "logs:DescribeLogGroups",
+                    "logs:DescribeLogStreams"
+                ],
+                "Resource": [
+                    "arn:aws:logs:us-east-1:<PRJ_ID>:log-group:/aws/bedrock/model-invocations:*",
+                    "arn:aws:logs:us-east-1:<PRJ_ID>:log-group:/aws/bedrock/model-invocations"
+                ]
+            }
+        ]
+    }'
 
 aws iam create-user --user-name math-iam-user
 aws iam attach-user-policy --user-name math-iam-user --policy-arn arn:aws:iam::aws:policy/AmazonBedrockFullAccess

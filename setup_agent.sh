a47bc1bdda550f99cb2aff1e3b5251e760f1571a
ankitsikkacts <ankit.sikka@cognizant.com>
2025-07-23 13:47:33 +0530

    Update setup_agent.sh

--- setup_agent.sh
+++ setup_agent.sh
@@ -2,6 +2,22 @@ aws iam create-user --user-name agent-iam-user
 aws iam attach-user-policy --user-name agent-iam-user --policy-arn arn:aws:iam::aws:policy/AmazonBedrockFullAccess
 aws iam create-access-key --user-name agent-iam-user
 
+
+aws iam put-user-policy \
+  --user-name agent-iam-user \
+  --policy-name AllowCloudWatchGetMetricData \
+  --policy-document '{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "cloudwatch:GetMetricData",
+        "Resource": "*"
+      }
+    ]
+  }'
+
+
 aws iam create-user --user-name math-iam-user
 aws iam attach-user-policy --user-name math-iam-user --policy-arn arn:aws:iam::aws:policy/AmazonBedrockFullAccess
 aws iam create-access-key --user-name math-iam-user

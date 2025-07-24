7d59aac552209966572fddf2258a244e11278710
ankitsikkacts <ankit.sikka@cognizant.com>
2025-07-24 12:08:18 +0530

    Update setup_agent.sh

--- setup_agent.sh
+++ setup_agent.sh
@@ -1,58 +1,46 @@
+#!/bin/bash
+
 aws iam create-user --user-name agent-iam-user
 aws iam attach-user-policy --user-name agent-iam-user --policy-arn arn:aws:iam::aws:policy/AmazonBedrockFullAccess
 aws iam create-access-key --user-name agent-iam-user
 
 
-#!/bin/bash
-
-# Define the first policy document
-POLICY_DOC_1=$(cat <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "cloudwatch:GetMetricData",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-)
-
-# Apply the first policy
+# Policy document for CloudWatch GetMetricData
 aws iam put-user-policy \
   --user-name agent-iam-user \
   --policy-name AllowCloudWatchGetMetricData \
-  --policy-document "$POLICY_DOC_1"
+  --policy-document '{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "cloudwatch:GetMetricData",
+        "Resource": "*"
+      }
+    ]
+  }'
 
-# Define the second policy document
-POLICY_DOC_2=$(cat <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:FilterLogEvents",
-        "logs:DescribeLogGroups",
-        "logs:DescribeLogStreams"
-      ],
-      "Resource": [
-        "arn:aws:logs:us-east-1:<PRJ_ID>:log-group:/aws/bedrock/model-invocations:*",
-        "arn:aws:logs:us-east-1:<PRJ_ID>:log-group:/aws/bedrock/model-invocations"
-      ]
-    }
-  ]
-}
-EOF
-)
-
-# Apply the second policy
+# Policy document for Bedrock Logs Access
 aws iam put-user-policy \
-  --user-name agent-iam-user \
-  --policy-name BedrockLogsAccess \
-  --policy-document "$POLICY_DOC_2"
+    --user-name agent-iam-user \
+    --policy-name BedrockLogsAccess \
+    --policy-document '{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "logs:FilterLogEvents",
+                    "logs:DescribeLogGroups",
+                    "logs:DescribeLogStreams"
+                ],
+                "Resource": [
+                    "arn:aws:logs:us-east-1:345208593967:log-group:/aws/bedrock/model-invocations:*",
+                    "arn:aws:logs:us-east-1:345208593967:log-group:/aws/bedrock/model-invocations"
+                ]
+            }
+        ]
+    }'
 
 
 aws iam create-user --user-name math-iam-user
